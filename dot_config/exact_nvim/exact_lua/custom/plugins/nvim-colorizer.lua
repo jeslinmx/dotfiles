a7ba4ee3227d53beef807ca7a03ad00344db5fdb
Jeshua Lin <jeslinmx@users.noreply.github.com>
2023-07-24 04:36:58 +0800

    nvim: added colorizer overrides

---
+++ dot_config/exact_nvim/exact_lua/custom/plugins/nvim-colorizer.lua
@@ -0,0 +1,11 @@
+---@type NvPluginSpec
+return {
+  "NvChad/nvim-colorizer.lua",
+  opts = {
+    RGB = true,
+    RRGGBB = true,
+    rgb_fn = true,
+    hsl_fn = true,
+    names = false,
+  },
+}
