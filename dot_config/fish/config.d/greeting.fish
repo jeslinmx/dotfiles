f54ba9691fb61d93a57b8b5e871ff72e9bb59048
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-03-10 21:13:16 +0800

    removed fish, nushell, and btop configs

--- dot_config/fish/config.d/greeting.fish
+++
@@ -1,7 +0,0 @@
-if [ $SHLVL -eq 1 ]
-    function fish_greeting
-        neofetch --config ~/.config/neofetch/simple.conf
-    end
-else
-    set fish_greeting ''
-end
--- dot_config/fish/config.d/vimode.fish
+++
@@ -1,4 +0,0 @@
-fish_vi_key_bindings
-set fish_cursor_visual block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
